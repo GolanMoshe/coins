44dfc8e92dc19357a7fc1d448e8d4b32ec720314
Alon Pinto <alon@getruck.co>
2022-07-03 22:00:32 +0300

    create CoinService moudle

--- src/services/data.coins.service.js
+++ src/services/data.coins.service.js
@@ -1,23 +1,31 @@
-const GET_ALL_COINS_URL = "https://api.coingecko.com/api/v3/coins/list";
-const GET_COIN_BY_ID_URL = "https://api.coingecko.com/api/v3/coins/:coinId";
+function CoinService() {
+  const GET_ALL_COINS_URL = "https://api.coingecko.com/api/v3/coins/list";
+  const GET_COIN_BY_ID_URL = "https://api.coingecko.com/api/v3/coins/:coinId";
 
-function getAllCoins() {
-  fetch(GET_ALL_COINS_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      const selectCoins = data.splice(0, 100);
-      console.log(selectCoins);
-      return selectCoins;
-    });
+  function getAllCoins() {
+    fetch(GET_ALL_COINS_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        const selectCoins = data.splice(0, 100);
+        console.log(selectCoins);
+        return selectCoins;
+      });
+  }
+
+  function getCoinById(coinId) {
+    const url = GET_COIN_BY_ID_URL.replace(":coinId", coinId);
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((selectedCoin) => {
+        console.log(selectedCoin);
+        return selectedCoin;
+      });
+  }
+  return {
+    getAllCoins,
+    getCoinById,
+  };
 }
 
-function getCoinById(coinId) {
-  const url = GET_COIN_BY_ID_URL.replace(":coinId", coinId);
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((selectedCoin) => {
-      console.log(selectedCoin);
-      return selectedCoin;
-    });
-}
+const coinService = CoinService();

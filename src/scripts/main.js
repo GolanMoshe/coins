872abb402f1021bbab35cb49943145b3abc03ef8
Pinto Alon <pintoalon@gmail.com>
2022-07-08 14:43:44 +0300

    rename

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,48 +1,32 @@
 const errorMessageContainer = document.getElementById('error-message-container');
-    const onLoad = async () => {
-      try {
-
-        const coinTemplate = document.getElementById('coin-template')
-        const coinListContainerRef = document.getElementById('coin-list-container');
-        const coins = (await coinService.getAllCoins()).slice(0, 100)
-
-        //const selectedCoins = await coinService.getSelectedCoins()
 
 
-
-        for (const coin of coins) {
-
-          //Best solution
-          const coinCardNode = coinTemplate.content.cloneNode(true);
-          const coinNameRef = coinCardNode.querySelector(".coin-name");
-
-          const coinCheckBoxRef = coinCardNode.querySelector(".card-body div.form-check input.form-check-input");
-          coinCheckBoxRef.setAttribute("data_coin_id", coin.id);
-          coinCheckBoxRef.onclick = (e) => {
-
-            const selectedCoin = e.target.attributes.data_coin_id.value;
-            console.log(`selectedCoin ${selectedCoin} ${e.target.checked}`)
-          }
-
-          coinNameRef.innerText = coin.name;
-          coinListContainerRef.appendChild(coinCardNode);
-
-          // First Solution
-          // append strings
-          //coinListContainerRef.innerHTML += template
+const onCoinSelectedStatisChanged = (e) => {
+  const selectedCoin = e.target.attributes.data_coin_id.value;
+  console.log(`selectedCoin ${selectedCoin} ${e.target.checked}`)
+}
 
 
-          // Second Solution  
-          //coinListContainerRef.insertAdjacentHTML('beforeend', coinHtmlTemplate);
-          //elements.push(coinHtmlTemplate);
-        }
+const onLoad = async () => {
+  try {
 
-        //coinListContainerRef.innerHTML = elements.join("");
+    const coinTemplate = document.getElementById('coin-template')
+    const coinListContainerRef = document.getElementById('coin-list-container');
+    const coins = (await coinService.getAllCoins()).slice(0, 100)
 
-      } catch (error) {
-        console.error(`problem getAllCoins`, error);
-        errorMessageContainer.innerText = error.message
-      }
-    };
+    for (const coin of coins) {
+      const coinCardNode = coinTemplate.content.cloneNode(true);
+      const coinNameRef = coinCardNode.querySelector(".coin-name");
 
- 
+      const coinCheckBoxRef = coinCardNode.querySelector(".card-body div.form-check input.form-check-input");
+      coinCheckBoxRef.setAttribute("data_coin_id", coin.id);
+      coinCheckBoxRef.onclick = onCoinSelectedStatisChanged
+      coinNameRef.innerText = coin.name;
+      coinListContainerRef.appendChild(coinCardNode);
+    }
+  } catch (error) {
+    console.error(`problem getAllCoins`, error);
+    errorMessageContainer.innerText = error.message
+  }
+};
+
